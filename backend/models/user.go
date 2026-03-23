7cb203b1ff6582d879d8516948d3b54f6ea774e0
aprimr <aprimregmi24@gmail.com>
2026-03-23 18:34:29 +0545

    add: user model

---
+++ backend/models/user.go
@@ -0,0 +1,35 @@
+package models
+
+import (
+	"time"
+)
+
+type User struct {
+	Uid        string    `json:"uid"`
+	Name       string    `json:"name"`
+	Username   string    `json:"username"`
+	Email      string    `json:"email"`
+	Password   string    `json:"-"`
+	Bio        string    `json:"bio"`
+	ProfileUrl string    `json:"profile_url"`
+	IsOnline   bool      `json:"is_online"`
+	LastSeen   time.Time `json:"last_seen"`
+	IsActive   bool      `json:"is_active"`
+	IsDeleted  bool      `json:"is_deleted"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
+}
+
+type RegisterInput struct {
+	Name       string `json:"name"`
+	Username   string `json:"username"`
+	Email      string `json:"email"`
+	Password   string `json:"password"`
+	Bio        string `json:"bio"`
+	ProfileUrl string `json:"profile_url"`
+}
+
+type LoginInput struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
