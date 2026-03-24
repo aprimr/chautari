fa070efe4c9b416b7610d5023240f916a851c20d
aprimr <aprimregmi24@gmail.com>
2026-03-24 10:24:07 +0545

    add: authentication middleware

---
+++ backend/middlewares/authentication.go
@@ -0,0 +1,39 @@
+package middlewares
+
+import (
+	"context"
+	"net/http"
+	"strings"
+
+	"github.com/aprimr/chautari/utils"
+	"github.com/aprimr/chautari/validation"
+)
+
+func Authentication(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		// Get auth header and trim prefix
+		authHeader := r.Header.Get("Authentication")
+		if validation.IsEmptyString(authHeader) {
+			utils.SendError(w, "JWT token missing", http.StatusUnauthorized)
+			return
+		}
+
+		jwtToken := strings.TrimPrefix(authHeader, "Bearer ")
+
+		// Verify JWT
+		jwtClaims, err := utils.VerifyToken(jwtToken)
+		if err != nil {
+			utils.SendError(w, "JWT token tampered", http.StatusUnauthorized)
+			return
+		}
+
+		// Send jwt with next request
+		ctx := context.WithValue(r.Context(), "uid", jwtClaims.Uid)
+		ctx = context.WithValue(ctx, "username", jwtClaims.Username)
+		ctx = context.WithValue(ctx, "email", jwtClaims.Email)
+
+		// call next handler
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
